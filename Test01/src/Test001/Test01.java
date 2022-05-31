e1278032a90d8103b67c408449709c500e8b89e7
Luke <tomwei1314@gmail.com>
2022-05-31 16:30:10 +0800

    3.5rd Version

--- Test01/src/Test001/Test01.java
+++ Test01/src/Test001/Test01.java
@@ -4,7 +4,7 @@ public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("2nd Version");
 		//我是組員，要修改檔案
-		System.out.println("3ed Version");
+		System.out.println("3rd Version");
 		int aaa;
 		System.out.println("你好");
 	}
